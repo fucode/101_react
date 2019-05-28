9818541b5d95e1e58107da37824387e2fccda0f7
Faraz <farazuddinva@gmail.com>
2019-05-28 10:33:29 -0400

    add Lotto

---
+++ lotto/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Lottery from './Lottery';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+      </div>
+    );
+  }
+}
+
+export default App;
